2418582a87809ce6ac07ee837b98f4d17e4ed6ec
Kelsey <kel.kelsey@gmail.com>
2020-08-18 12:53:27 -0300

    Criado método createProduct - Observable

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,6 +10,11 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    quantity: null
+  }
+
   constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
@@ -16,7 +22,11 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct():void {
-    this.productService.showMessage('Produto criado.')
+
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado.')
+      this.router.navigate(['/products'])
+    });
   }
 
   cancel():void {
